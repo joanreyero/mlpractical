ce56c1ef8b45b4c02f08f1280b387598f3e9cfa7
Joan Reyero <joanreyero@gmail.com>
2020-11-23 17:31:12 +0000

    Added first draft of batch norm

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -187,6 +187,59 @@ class ConvolutionalProcessingBlock(nn.Module):
         return out
 
 
+class NormConvolutionalProcessingBlock(nn.Module):
+    def __init__(self, input_shape, num_filters, kernel_size, padding, bias, dilation):
+        super(ConvolutionalProcessingBlock, self).__init__()
+
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.input_shape = input_shape
+        self.padding = padding
+        self.bias = bias
+        self.dilation = dilation
+
+        self.build_module()
+
+    def build_module(self):
+        self.layer_dict = nn.ModuleDict()
+        x = torch.zeros(self.input_shape)
+        out = x
+
+        self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
+                                              kernel_size=self.kernel_size, dilation=self.dilation,
+                                              padding=self.padding, stride=1)
+
+        self.layer_dict['bn_0'] = nn.BatchNorm2d(num_features=out.shape[1])
+
+        out = self.layer_dict['conv_0'].forward(out)
+
+        out = F.leaky_relu(out)
+
+        self.layer_dict['conv_1'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
+                                              kernel_size=self.kernel_size, dilation=self.dilation,
+                                              padding=self.padding, stride=1)
+
+        self.layer_dict['bn_1'] = nn.BatchNorm2d(num_features=out.shape[1])
+
+        out = self.layer_dict['conv_1'].forward(out)
+        out = F.leaky_relu(out)
+
+        print(out.shape)
+
+    def forward(self, x):
+        out = x
+
+        out = self.layer_dict['conv_0'].forward(out)
+        out - self.layer_dict['bn_0'].forward(out)
+        out = F.leaky_relu(out)
+
+        out = self.layer_dict['conv_1'].forward(out)
+        out - self.layer_dict['bn_1'].forward(out)
+        out = F.leaky_relu(out)
+
+        return out
+
+
 class ConvolutionalDimensionalityReductionBlock(nn.Module):
     def __init__(self, input_shape, num_filters, kernel_size, padding, bias, dilation, reduction_factor):
         super(ConvolutionalDimensionalityReductionBlock, self).__init__()
